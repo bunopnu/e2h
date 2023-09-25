42a85c690d2328eddb36f58902ddabb9bdc5ebb0
bun <git@bun.rip>
2023-09-25 22:01:11 +0300

    use binary construction for generating html

--- src/e2h.erl
+++ src/e2h.erl
@@ -5,26 +5,30 @@
 -type attributes() :: [{binary(), binary()}].
 -type elements() :: [{binary(), attributes(), elements()} | binary()].
 
--spec encode_attributes(attributes()) -> iodata().
+-spec encode_attributes(attributes()) -> binary().
 encode_attributes(Attributes) when is_list(Attributes) ->
-    encode_attributes(Attributes, []).
+    encode_attributes(Attributes, <<>>).
 
--spec encode_attributes(attributes(), list()) -> iodata().
+-spec encode_attributes(attributes(), binary()) -> binary().
+encode_attributes([{Key, Value} | Tail], Acc) when is_binary(Key), is_binary(Value) ->
+    EncodedAttribute = <<" ", Key/binary, "=\"", Value/binary, "\"">>,
+    encode_attributes(Tail, <<Acc/binary, EncodedAttribute/binary>>);
 encode_attributes([], Acc) ->
-    lists:reverse(Acc);
-encode_attributes([{Key, Value} | Tail], Acc) ->
-    encode_attributes(Tail, ["\"", Value, "=\"", Key, " " | Acc]).
+    Acc.
 
--spec encode_elements(elements()) -> iodata().
-encode_elements(Elements) ->
-    encode_elements(Elements, []).
+-spec encode_elements(elements()) -> binary().
+encode_elements(Elements) when is_list(Elements) ->
+    encode_elements(Elements, <<>>).
 
--spec encode_elements(elements(), list()) -> iodata().
-encode_elements([], Acc) ->
-    lists:reverse(Acc);
+-spec encode_elements(elements(), binary()) -> binary().
 encode_elements([Raw | Tail], Acc) when is_binary(Raw) ->
-    encode_elements(Tail, [Raw | Acc]);
-encode_elements([{Tag, Attributes, Value} | Tail], Acc) ->
+    encode_elements(Tail, <<Acc/binary, Raw/binary>>);
+encode_elements([{Tag, Attributes, Value} | Tail], Acc) when is_binary(Tag) ->
     EncodedValue = encode_elements(Value),
     EncodedAttributes = encode_attributes(Attributes),
-    encode_elements(Tail, [">", Tag, "</", EncodedValue, ">", EncodedAttributes, Tag, "<" | Acc]).
+    EncodedElement =
+        <<"<", Tag/binary, EncodedAttributes/binary, ">", EncodedValue/binary, "</", Tag/binary,
+            ">">>,
+    encode_elements(Tail, <<Acc/binary, EncodedElement/binary>>);
+encode_elements([], Acc) ->
+    Acc.
