6320a0b1f418f517a7e41574f5fafcc813a0c26d
bun <git@bun.rip>
2023-09-25 22:29:19 +0300

    add `escape/1` for security

--- src/e2h.erl
+++ src/e2h.erl
@@ -1,14 +1,34 @@
 -module(e2h).
 
--export([render/1]).
+-export([render/1, escape/1]).
 -export_type([attributes/0, elements/0]).
 
 -type attributes() :: [{binary(), binary()}].
 -type elements() :: [{binary(), attributes(), elements()} | binary()].
 
 -spec render(elements()) -> binary().
-render(Elements) ->
+render(Elements) when is_list(Elements) ->
     encode_elements(Elements, <<"<!DOCTYPE html>\n">>).
+
+-spec escape(binary()) -> binary().
+escape(Data) when is_binary(Data) ->
+    escape(Data, <<>>).
+
+-spec escape(binary(), binary()) -> binary().
+escape(<<$<, Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, "&lt;">>);
+escape(<<$>, Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, "&gt;">>);
+escape(<<$&, Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, "&amp;">>);
+escape(<<$", Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, "&quot;">>);
+escape(<<$', Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, "&#x27;">>);
+escape(<<Head, Tail/binary>>, Acc) ->
+    escape(Tail, <<Acc/binary, Head>>);
+escape(<<>>, Acc) ->
+    Acc.
 
 -spec encode_attributes(attributes()) -> binary().
 encode_attributes(Attributes) when is_list(Attributes) ->
@@ -16,7 +36,7 @@ encode_attributes(Attributes) when is_list(Attributes) ->
 
 -spec encode_attributes(attributes(), binary()) -> binary().
 encode_attributes([{Key, Value} | Tail], Acc) when is_binary(Key), is_binary(Value) ->
-    EncodedAttribute = <<" ", Key/binary, "=\"", Value/binary, "\"">>,
+    EncodedAttribute = <<$\s, Key/binary, "=\"", Value/binary, $">>,
     encode_attributes(Tail, <<Acc/binary, EncodedAttribute/binary>>);
 encode_attributes([], Acc) ->
     Acc.
@@ -31,9 +51,9 @@ encode_elements([Raw | Tail], Acc) when is_binary(Raw) ->
 encode_elements([{Tag, Attributes, Value} | Tail], Acc) when is_binary(Tag) ->
     EncodedValue = encode_elements(Value),
     EncodedAttributes = encode_attributes(Attributes),
-    EncodedElement =
-        <<"<", Tag/binary, EncodedAttributes/binary, ">", EncodedValue/binary, "</", Tag/binary,
-            ">">>,
+    EncodedElement = <<
+        $<, Tag/binary, EncodedAttributes/binary, $>, EncodedValue/binary, "</", Tag/binary, $>
+    >>,
     encode_elements(Tail, <<Acc/binary, EncodedElement/binary>>);
 encode_elements([], Acc) ->
     Acc.
